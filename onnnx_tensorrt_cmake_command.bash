85361e9e61f20c4bc74e147127d7f837cc49e027
Andrei Pokrovsky <andrei.pokrovsky@luminartech.com>
2019-08-30 12:58:21 -0700

    Add support for Xavier in cmake bash file

--- onnnx_tensorrt_cmake_command.bash
+++ onnnx_tensorrt_cmake_command.bash
@@ -1,3 +1,7 @@
-cmake .. -DTENSORRT_ROOT=/home/andrei/Downloads/nvidia/trt/TensorRT-5.1.5.0/ -DCUDA_INCLUDE_DIRS=/usr/local/cuda-10.0/include -DCMAKE_BUILD_TYPE=Debug -DGPU_ARCHS="60 70"
+# x64 build
+#cmake .. -DTENSORRT_ROOT=/home/andrei/Downloads/nvidia/trt/TensorRT-5.1.5.0/ -DCUDA_INCLUDE_DIRS=/usr/local/cuda-10.0/include -DCMAKE_BUILD_TYPE=Debug -DGPU_ARCHS="60 70"
+
+# Xavier build
+cmake .. -DCUDA_INCLUDE_DIRS=/usr/local/cuda-10.0/include -DGPU_ARCHS=70
 
 
